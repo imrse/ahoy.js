eb8212afebf1ea5359125b0582d6af68cf00cb95
Kristian Kvarnsbacke <kristian@kmja.com>
2023-12-18 18:57:17 +0100

    Use localStorage instead of document.cookie

--- src/cookies.js
+++ src/cookies.js
@@ -1,32 +1,34 @@
 // https://www.quirksmode.org/js/cookies.html
 
 export default {
-  set: function (name, value, ttl, domain) {
-    let expires = "";
-    let cookieDomain = "";
-    if (ttl) {
-      const date = new Date();
-      date.setTime(date.getTime() + (ttl * 60 * 1000));
-      expires = "; expires=" + date.toGMTString();
-    }
-    if (domain) {
-      cookieDomain = "; domain=" + domain;
-    }
-    document.cookie = name + "=" + escape(value) + expires + cookieDomain + "; path=/; samesite=lax";
+  set: function (name, value, ttl) {
+    const now = new Date();
+    // `ttl` is in minutes, convert it to milliseconds
+    const expirationTime = now.getTime() + ttl * 60000;
+
+    const item = {
+      value: value,
+      expires: expirationTime
+    };
+
+    localStorage.setItem(name, JSON.stringify(item));
   },
   get: function (name) {
-    let i, c;
-    const nameEQ = name + "=";
-    const ca = document.cookie.split(';');
-    for (i = 0; i < ca.length; i++) {
-      c = ca[i];
-      while (c.charAt(0) === ' ') {
-        c = c.substring(1, c.length);
-      }
-      if (c.indexOf(nameEQ) === 0) {
-        return unescape(c.substring(nameEQ.length, c.length));
-      }
+    const itemStr = localStorage.getItem(name);
+    if (!itemStr) {
+      return null;
     }
-    return null;
+
+    const item = JSON.parse(itemStr);
+    const now = new Date();
+
+    // Check if the item has expired
+    if (now.getTime() > item.expires) {
+      // If expired, remove it from storage and return null
+      localStorage.removeItem(name);
+      return null;
+    }
+
+    return item.value;
   }
 };
